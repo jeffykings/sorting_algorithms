96f7d38f3da925f522c0c5d88a64c86dc035fd6c
jeffykings <jeffykings@gmail.com>
2025-03-21 18:29:42 +0100

    task1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,7 +4,7 @@
  * insertion_sort_list -  that sorts a doubly linked list of integers
  * in ascending order using the Insertion sort algorithm
  *
- * @list: the singly linked list to be sorted
+ * @list: the doubly linked list to be sorted
  */
 
 void insertion_sort_list(listint_t **list)
@@ -23,9 +23,12 @@ void insertion_sort_list(listint_t **list)
 		{
 			if (first->prev != NULL)
 				first->prev->next = second;
+			else
+				*list = second;
+
 			if (second->next != NULL)
 				second->next->prev = first;
-			
+
 			first->next = second->next;
 			second->prev = first->prev;
 			first->prev = second;
@@ -34,8 +37,9 @@ void insertion_sort_list(listint_t **list)
 			swap = 1;
 			print_list(*list);
 		}
-	}
 
 		if (swap == 0)
 			temp = temp->next;
+
+	}
 }
